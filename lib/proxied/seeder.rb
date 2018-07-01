8f45b158d95e52b378764960ac9c2c4eefb05a69
Sebastian <sebastian.johnsson@gmail.com>
2018-07-01 18:07:11 +0200

    Update seeder, class method + additional fields

--- lib/proxied/seeder.rb
+++ lib/proxied/seeder.rb
@@ -8,11 +8,13 @@ module Proxied
     #   protocol (http,socks)
     #   proxy_type (private,public)
     #   category
+    #   country
+    #   city
     #   username
     #   password
     # }
     
-    def import(proxies)
+    def self.import(proxies)
       proxies.each do |proxy_item|
         query                       =   {
           host:       proxy_item[:ip_address]&.to_s&.strip,
@@ -23,6 +25,8 @@ module Proxied
           protocol:   proxy_item.fetch(:protocol, "http")&.to_s&.strip&.downcase,
           proxy_type: proxy_item.fetch(:type, :private)&.to_s&.strip,
           category:   proxy_item.fetch(:category, nil)&.to_s&.strip&.downcase,
+          country:    proxy_item.fetch(:country, nil)&.to_s&.strip&.downcase,
+          city:       proxy_item.fetch(:city, nil)&.to_s&.strip&.downcase,
           username:   proxy_item.fetch(:username, nil)&.to_s&.strip,
           password:   proxy_item.fetch(:password, nil)&.to_s&.strip
         }.merge(query)
