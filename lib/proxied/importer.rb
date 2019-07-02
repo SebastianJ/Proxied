2be033d6f1eaffe8f58307b4d5e78d097051505f
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2019-07-02 18:28:11 +0200

    Update importer

--- lib/proxied/importer.rb
+++ lib/proxied/importer.rb
@@ -25,7 +25,7 @@ module Proxied
           protocol:   proxy_item.fetch(:protocol, "http")&.to_s&.strip&.downcase,
           proxy_type: proxy_item.fetch(:type, :private)&.to_s&.strip,
           category:   proxy_item.fetch(:category, nil)&.to_s&.strip&.downcase,
-          country:    proxy_item.fetch(:country, nil)&.to_s&.strip&.downcase,
+          country:    proxy_item.fetch(:country, nil)&.to_s&.strip&.upcase,
           city:       proxy_item.fetch(:city, nil)&.to_s&.strip&.downcase,
           username:   proxy_item.fetch(:username, nil)&.to_s&.strip,
           password:   proxy_item.fetch(:password, nil)&.to_s&.strip
